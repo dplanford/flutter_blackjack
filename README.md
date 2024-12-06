370c1fe0f2c79aafc5d72d272a2a2078c80e8b2a
Doug Lanford <opus@opusgames.com>
2024-12-06 13:47:55 -0700

    Switched back to a single card, with card back.

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 A small playfield to try oul Flutter UI with animations... the ultimate idea is to add a package with a safe few card games (no actual gambling, but ten rounds of virtual Blackjack, Texas Hold-em, etc.).
 
-This is also a first testbed of 3D flippy/spinny Flutter animations using 2D graphics....
+This is a first testbed of 3D flippy/spinny Flutter animations using 2D graphics....
+
+To play with this, you will need to create a new Flutter project, and then copy these files over the existing boilerplate files.
--- lib/widget/playing_card_deck_spinny_widget.dart
+++ lib/widget/playing_card_deck_spinny_widget.dart
@@ -8,7 +8,7 @@ import 'package:blackjack_package/helper/playing_card_helper.dart';
 import 'package:blackjack_package/widget/playing_card_widget.dart';
 
 const double piDiv2 = pi / 2.0;
-const double pi2 = 2 * pi;
+const double pi2 = 2.0 * pi;
 
 class PlayingCardDeckSpinnyWidget extends StatefulWidget {
   const PlayingCardDeckSpinnyWidget({super.key});
@@ -22,7 +22,7 @@ class _PlayingCardDeckSpinnyWidgetState extends State<PlayingCardDeckSpinnyWidge
   double _rotationX = piDiv2;
   double _rotationZ = 0;
   int _cardIndex = 0;
-  final bool _backShowing = false;
+  bool _backShowing = false;
 
   @override
   void initState() {
@@ -30,11 +30,11 @@ class _PlayingCardDeckSpinnyWidgetState extends State<PlayingCardDeckSpinnyWidge
 
     _myDeck = PlayingCard.getShuffledSingleDeck();
 
-    _rotationX = RandomHelper.returnRandomDouble(min: -180.0, max: 180.0);
-    _rotationZ = RandomHelper.returnRandomDouble(min: -180.0, max: 180.0);
-    //if (_rotationX > -piDiv2 && _rotationX < piDiv2) {
-    //  _backShowing = false;
-    //}
+    _rotationX = RandomHelper.returnRandomDouble(min: -piDiv2, max: piDiv2);
+    _rotationZ = RandomHelper.returnRandomDouble(min: -piDiv2, max: piDiv2);
+    if (_rotationX > -piDiv2 && _rotationX < piDiv2) {
+      _backShowing = false;
+    }
 
     // TODO: Update timer is set to 60 updates per second (16667 microseconds)...
     // This seems to work pretty smoothly as a Flutter gaming animation update loop?
@@ -46,12 +46,10 @@ class _PlayingCardDeckSpinnyWidgetState extends State<PlayingCardDeckSpinnyWidge
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: PlayingCardWidget(
-        card: _backShowing ? null : _myDeck[_cardIndex],
-        rotationX: _rotationX,
-        rotationZ: _rotationZ,
-      ),
+    return PlayingCardWidget(
+      card: _backShowing ? null : _myDeck[_cardIndex],
+      rotationX: _rotationX,
+      rotationZ: _rotationZ,
     );
   }
 
@@ -64,21 +62,21 @@ class _PlayingCardDeckSpinnyWidgetState extends State<PlayingCardDeckSpinnyWidge
 
       _rotationZ += 0.005;
       if (_rotationZ > pi) {
-        _rotationZ += pi2;
+        _rotationZ -= pi2;
       }
 
       if (_rotationX > -piDiv2 && _rotationX < piDiv2) {
-        //if (_backShowing) {
-        //  _backShowing = false;
-        if (_cardIndex >= _myDeck.length - 1) {
-          _cardIndex = 0;
-        } else {
-          _cardIndex += 1;
+        if (_backShowing) {
+          _backShowing = false;
+          if (_cardIndex >= _myDeck.length - 1) {
+            _cardIndex = 0;
+          } else {
+            _cardIndex += 1;
+          }
         }
+      } else if (!_backShowing) {
+        _backShowing = true;
       }
-      //} else if (!_backShowing) {
-      //  _backShowing = true;
-      //}
     });
   }
 }
